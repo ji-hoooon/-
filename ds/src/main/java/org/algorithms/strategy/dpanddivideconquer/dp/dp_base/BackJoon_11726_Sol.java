b49799f2bde8be1e117f984c9f550ff6df44e752
ji-hoooon <ljh6384@naver.com>
2023-05-05 18:24:49 +0900

    DP (1) 완료

---
+++ ds/src/main/java/org/algorithms/strategy/dpanddivideconquer/dp/dp_base/BackJoon_11726_Sol.java
@@ -0,0 +1,87 @@
+package org.algorithms.strategy.dpanddivideconquer.dp.dp_base;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+//2xn 타일링
+public class BackJoon_11726_Sol {
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+
+    static int N;
+    static int[] Dy;
+
+    static void input() {
+        N = scan.nextInt();
+    }
+
+    static void pro() {
+        Dy = new int[1005];
+        // 초기값 구하기
+        Dy[1] = 1;
+        Dy[2] = 2;
+
+        // 점화식을 토대로 Dy 배열 채우기
+        for (int i = 3; i <= N; i++) {
+            Dy[i] = (Dy[i - 1] + Dy[i - 2]) % 10007;
+        }
+        System.out.println(Dy[N]);
+    }
+
+    public static void main(String[] args) {
+        input();
+        pro();
+    }
+
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public FastReader(String s) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
